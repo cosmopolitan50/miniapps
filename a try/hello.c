f1d120c0060726a53a205a48f2118a747cb2df36
Lzn <1162253726@qq.com>
2021-08-26 14:50:01 +0800

    complete

--- a try/hello.c
+++ a try/hello.c
@@ -2,11 +2,6 @@
 using namespace std;
 int main()
 {
-	cout<<"Hello World!"<<endl;
-<<<<<<< HEAD
-	
-=======
-	cout<<"zzn is _____."
+	cout<<"大家一起编写代码吧！"<<endl;
 	return 0;
->>>>>>> 021cf69d64b9ef4ff0d92cc5c3f76d9d6fed67f0
 }
